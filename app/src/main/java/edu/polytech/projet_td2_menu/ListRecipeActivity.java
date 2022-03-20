67b9523767c9e92a99f8eadab6027bb009155397
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-03-20 13:57:58 +0100

    Vue de ListRecipeActivity

---
+++ app/src/main/java/edu/polytech/projet_td2_menu/ListRecipeActivity.java
@@ -0,0 +1,14 @@
+package edu.polytech.projet_td2_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListRecipeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_recipe);
+    }
+}
