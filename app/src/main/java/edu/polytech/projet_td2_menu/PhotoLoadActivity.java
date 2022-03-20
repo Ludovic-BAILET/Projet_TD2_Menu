146400c9ed14f7d849a10334cf3cb8fe60088708
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-03-20 17:44:22 +0100

    ajout de la vue de PhotoLoadActivity

---
+++ app/src/main/java/edu/polytech/projet_td2_menu/PhotoLoadActivity.java
@@ -0,0 +1,14 @@
+package edu.polytech.projet_td2_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoLoadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_load);
+    }
+}
