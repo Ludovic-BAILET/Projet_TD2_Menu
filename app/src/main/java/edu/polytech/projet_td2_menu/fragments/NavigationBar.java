0ae33e9e8c8fe6676baf030fa25483c6301b20d0
Quentin Dubois <quentin.dubois@etu.univ-cotedazur.fr>
2022-03-25 10:05:49 +0100

    ajout des listeners sur la barre de navigation (sauf sur Mes Courses et Recettes)

--- app/src/main/java/edu/polytech/projet_td2_menu/fragments/NavigationBar.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/fragments/NavigationBar.java
@@ -1,12 +1,16 @@
 package edu.polytech.projet_td2_menu.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
 
+import edu.polytech.projet_td2_menu.ListRecipeActivity;
+import edu.polytech.projet_td2_menu.PlanningActivity;
 import edu.polytech.projet_td2_menu.R;
 
 public class NavigationBar extends Fragment {
@@ -14,6 +18,28 @@ public class NavigationBar extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.navigation_bar, container, false);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.navigation_bar, container, false);
+
+        //Listener de Planning
+        layout.findViewById(R.id.planning).setOnClickListener(click -> {
+            startActivity(new Intent(getContext(), PlanningActivity.class));
+        });
+
+        //Listener de Recettes
+        layout.findViewById(R.id.recettes).setOnClickListener(click -> {
+            startActivity(new Intent(getContext(), ListRecipeActivity.class));
+        });
+
+        //Listener de Mes Courses
+        layout.findViewById(R.id.mes_courses).setOnClickListener(click -> {
+            startActivity(new Intent(getContext(), PlanningActivity.class));
+        });
+
+        //Listener de Profil
+        layout.findViewById(R.id.profil).setOnClickListener(click -> {
+            startActivity(new Intent(getContext(), PlanningActivity.class));
+        });
+
+        return layout;
     }
 }
