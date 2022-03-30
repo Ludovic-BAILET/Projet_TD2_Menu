fa2bb77beefa4286cea63539cecb80770dbcbcd6
bastien <bastien.bezes@etu.unice.fr>
2022-03-30 15:03:35 +0200

    rename d'une layout

--- app/src/main/java/edu/polytech/projet_td2_menu/ProfilActivity.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/ProfilActivity.java
@@ -9,6 +9,6 @@ public class ProfilActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profil);
+        setContentView(R.layout.activity_profil);
     }
 }
