25d135e114dabdb49d043b21e8588fdb58e5dcff
bastien <bastien.bezes@etu.unice.fr>
2022-04-06 17:21:19 +0200

    photo 1 try

--- app/src/main/java/edu/polytech/projet_td2_menu/ProfilActivity.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/ProfilActivity.java
@@ -2,13 +2,28 @@ package edu.polytech.projet_td2_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfilActivity extends AppCompatActivity {
+
+    private Button buttonTMP;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+        buttonTMP = (Button) findViewById(R.id.tmp_camera);
+        buttonTMP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProfilActivity.this,PhotoLoadActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
