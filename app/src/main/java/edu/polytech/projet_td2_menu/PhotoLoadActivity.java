b1cc76ff68bcb4369165f8c41bbaca86b59180e3
bastien <bastien.bezes@etu.unice.fr>
2022-04-20 15:24:31 +0200

    prendre une photo fini
    photo load permet de prendre une photo avec la camera lorsqu'on appuie sur l'icone de camera , une fois la photo prise l'image peut etre vu sur l'ancien emplacement de l'icone de camera

--- app/src/main/java/edu/polytech/projet_td2_menu/PhotoLoadActivity.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/PhotoLoadActivity.java
@@ -1,23 +1,43 @@
 package edu.polytech.projet_td2_menu;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class PhotoLoadActivity extends AppCompatActivity {
 
     private Button valider;
-    private ImageView image;
+    private ImageView camera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_load);
 
-        valider = (Button) findViewById(R.id.button_valider;
-        
+        valider = (Button) findViewById(R.id.button_valider);
+        camera = findViewById(R.id.camera);
 
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(open_camera,100);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap photo = (Bitmap)data.getExtras().get("data");
+        camera.setImageBitmap(photo);
     }
 }
