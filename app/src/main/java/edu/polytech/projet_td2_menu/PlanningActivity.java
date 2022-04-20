50244409b12d203d287fae783a6ba59b5b0b4ca6
bastien <bastien.bezes@etu.unice.fr>
2022-04-20 17:32:35 +0200

    ajout debut du calendrier (non fonctionnel)
    ajout d'un emoji fichier dans planning pour ajouter un event dans l'agenda , non fonctionnel pour l'instant

--- app/src/main/java/edu/polytech/projet_td2_menu/PlanningActivity.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/PlanningActivity.java
@@ -2,12 +2,19 @@ package edu.polytech.projet_td2_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class PlanningActivity extends AppCompatActivity {
 
@@ -15,12 +22,16 @@ public class PlanningActivity extends AppCompatActivity {
     private TextView theDate;
     private ImageView imageCalendar;
 
+    private ImageView addToAgenda;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_planning);
         theDate = findViewById(R.id.date);
         imageCalendar = findViewById(R.id.calendar);
+        addToAgenda = findViewById(R.id.addToAgenda);
+
 
         Intent incomingIntent = getIntent();
         String date = incomingIntent.getStringExtra("date");
@@ -33,6 +44,28 @@ public class PlanningActivity extends AppCompatActivity {
         findViewById(R.id.bell).setOnClickListener(click -> {
             startActivity(new Intent(this, NotificationsCenterActivity.class));
         });
+
+
+        addToAgenda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ContentResolver cr  = getApplicationContext().getContentResolver();
+                ContentValues cv = new ContentValues();
+                cv.put(CalendarContract.Events.TITLE,"test title");
+                cv.put(CalendarContract.Events.DESCRIPTION,"test description");
+                cv.put(CalendarContract.Events.EVENT_LOCATION,"test event_location");
+                cv.put(CalendarContract.Events.DTSTART, Calendar.getInstance().getTimeInMillis());
+                cv.put(CalendarContract.Events.DTEND,Calendar.getInstance().getTimeInMillis()+60*60*1000);
+                cv.put(CalendarContract.Events.CALENDAR_ID,1);
+                cv.put(CalendarContract.Events.EVENT_TIMEZONE,Calendar.getInstance().getTimeZone().getID());
+
+                Uri uri = cr.insert(CalendarContract.Events.CONTENT_URI,cv);
+
+                Toast.makeText(getApplicationContext(),"Event is successfully added",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 
 
