7e2f052f594b96a6ff50eb72ac9ae8500352378d
bastien <bastien.bezes@etu.unice.fr>
2022-04-20 16:23:07 +0200

    ajout des permissions + prendre une photo dans la gallery
    pour prendre une photo avec la camera ou dans la gallerie il faut donner l'autorisation

--- app/src/main/java/edu/polytech/projet_td2_menu/PhotoLoadActivity.java
+++ app/src/main/java/edu/polytech/projet_td2_menu/PhotoLoadActivity.java
@@ -1,20 +1,38 @@
 package edu.polytech.projet_td2_menu;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PhotoLoadActivity extends AppCompatActivity {
 
+    private static final String TAG = "photoLoad";
     private Button valider;
     private ImageView camera;
+    private ImageView file;
+
+    private static final int IMAGE_PICK_CODE = 1000;
+    private static final int PERMISSION_PICK_CODE = 1001;
+
+    private static final int CAMERA_CODE = 100;
+    private static final int PERMISSION_CAMERA_CODE = 101;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,21 +41,111 @@ public class PhotoLoadActivity extends AppCompatActivity {
 
         valider = (Button) findViewById(R.id.button_valider);
         camera = findViewById(R.id.camera);
+        file = findViewById(R.id.file);
+
 
         camera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(open_camera,100);
+
+
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED){
+                        //permission not granted
+                        String[] permissions = {Manifest.permission.CAMERA};
+                        //show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_CAMERA_CODE);
+
+                    }else{
+                        //permission already granted
+                        takeImageFromCamera();
+                    }
+                }
+                else{
+                    //system os is less than marsmallow
+                    takeImageFromCamera();
+                }
+            }
+        });
+
+
+
+
+        file.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                        //permission not granted
+                        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                        //show popup for runtime permission
+                        requestPermissions(permissions, PERMISSION_PICK_CODE);
+
+                    }else{
+                        //permission already granted
+                        pickImageFromGallery();
+                    }
+                }
+                else{
+                    //system os is less than marsmallow
+                    pickImageFromGallery();
+                }
             }
         });
 
     }
 
+    private void takeImageFromCamera(){
+        Intent open_camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(open_camera,CAMERA_CODE);
+    }
+
+
+    private void pickImageFromGallery(){
+        //intent to pick image
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,IMAGE_PICK_CODE);
+    }
+
+
+    //handle result of runtime permission
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case PERMISSION_PICK_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission was granted
+                    pickImageFromGallery();
+                }else{
+                    //permission was denied
+                    Toast.makeText(this,"Permission denied ... !",Toast.LENGTH_SHORT).show();
+                }
+            }
+            case PERMISSION_CAMERA_CODE:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //permission was granted
+                    takeImageFromCamera();
+                }else{
+                    //permission was denied
+                    Toast.makeText(this,"Permission denied ... !",Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Bitmap photo = (Bitmap)data.getExtras().get("data");
-        camera.setImageBitmap(photo);
+
+        if(resultCode == RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            file.setImageURI(data.getData());
+        }
+
+        if(resultCode == RESULT_OK && requestCode == CAMERA_CODE){
+            Bitmap photo = (Bitmap)data.getExtras().get("data");
+            camera.setImageBitmap(photo);
+        }
+
     }
 }
